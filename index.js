a1b151a0ddb1632bb22a458e71fde67f7eb076d9
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-05-27 09:07:06 +0700

    change

--- index.js
+++ index.js
@@ -3,24 +3,9 @@ const server = jsonServer.create()
 const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
-// Set default middlewares (logger, static, cors and no-cache)
+
 server.use(middlewares)
 
-// Add custom routes before JSON Server router
-
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-
-// Use default router
 server.use(router)
 server.listen(port, () => {
   console.log('JSON Server is running')
